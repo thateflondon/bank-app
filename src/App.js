784078e0b00c97f04ea9bb52c08873c6d40942aa
Jerry Frederic <jerry.fpl@gmail.com>
2021-04-22 23:21:20 +0200

    Styled-components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Yup</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Nav, NavbarContainer, NavLogo } from "./NavbarElements";
+
+function Navbar() {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo>ECO</NavLogo>
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
+}
+
+export default Navbar;
