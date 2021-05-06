3d64ff930af3bbcaa5bcd6d850d2717a85f5d657
Jerry Frederic <jerry.fpl@gmail.com>
2021-05-06 02:58:25 +0200

    Pricing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Home from "./pages";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SignIn from "./components/SignIn";
+import Pricing from "./components/Pricing";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/signin" component={SignIn} exact />
+        <Route path="/pricing" component={Pricing} exact />
       </Switch>
       {/* <Home /> */}
     </Router>
